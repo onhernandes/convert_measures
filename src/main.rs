0b936bef87608252400634d0a7158adc2f4c662b
Matheus Hernandes <12698656+onhernandes@users.noreply.github.com>
2022-10-13 20:45:33 -0300

    Declare arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Current measure
+   #[arg(short, long)]
+   from: String,
+
+   /// Desired measure
+   #[arg(short, long)]
+   to: String,
+
+   /// Value
+   value: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+   let args = Args::parse();
+   print!("value: {:?}", args.value)
 }
